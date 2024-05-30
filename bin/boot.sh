4f60fcada19f2570f8f33aba186bbc91082f03ac
AndreasRaspberry <andreas.c.gade@gmail.com>
2024-05-31 00:09:59 +0200

    Fixed servo angle issue. Added additional scripts to run on start-up on raspberry

--- bin/boot.sh
+++ bin/boot.sh
@@ -4,3 +4,7 @@ sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 /home/emli/mqttSubPressure.sh > /dev/null 2>&1 &
 /home/emli/start_local_webserver.sh > /dev/null 2>&1 &
 /home/emli/picEverySec.sh > /dev/null 2>&1 &
+sudo -uemli python3 /home/emli/flaskserver.py > /dev/null 2>&1 &
+/home/emli/serialListen.sh > /dev/null 2>&1 &
+/home/emli/mqttSubRainDetect.sh > /dev/null 2>&1 &
+/home/emli/mqttSubRainWipeSerialWrite.sh > /dev/null 2>&1 &
--- mqttSubRainDetect.sh
+++ mqttSubRainDetect.sh
@@ -13,6 +13,6 @@ QOS="0"
 mosquitto_sub -h $BROKER -u $USER -P $PWD -t $TOPIC -q $QOS | while read -r message; do
     echo "Received message: $message"
     mosquitto_pub -h $BROKER -u $USER -P $PWD -t $TOPICPUB -m '{"wiper_angle": 0}'
-    mosquitto_pub -h $BROKER -u $USER -P $PWD -t $TOPICPUB -m '{"wiper_angle": 180}'
+    mosquitto_pub -h $BROKER -u $USER -P $PWD -t $TOPICPUB -m '{"wiper_angle": 90}'
     mosquitto_pub -h $BROKER -u $USER -P $PWD -t $TOPICPUB -m '{"wiper_angle": 0}'
 done
